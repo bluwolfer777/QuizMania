66933e8c5f9bd12749dfd2fb45737faed12c4886
bluwolfer777 <leonleonleonr@gmail.com>
2024-03-11 15:13:41 +0100

    working get session data

--- web/QuizMania/app.py
+++ web/QuizMania/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template_string, request, session, redirect, url_for, render_template
 from flask_session import Session
 import qrcode
-
+import time
 
 app = Flask(__name__, static_url_path='/static')
-
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 def generateQR(link,filename):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -14,10 +15,13 @@ def generateQR(link,filename):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save('static/' + filename + '.png')
 
+def generateSessionId(email):
+    id = str(time.time())
+    id += email
+    return id
 
 @app.route('/')
 def main_page():  # put application's code here
-
     return "<img src='/static/qr.png'>"
 
 @app.route('/play/')
@@ -37,6 +41,7 @@ def guestForm():
         email = request.form.get("email")
         newsletter = request.form.get("newsletter")
         print(first_name, last_name, email, newsletter)
+        session['id'] = generateSessionId(email)
     return render_template("guestForm.html")
 
 
