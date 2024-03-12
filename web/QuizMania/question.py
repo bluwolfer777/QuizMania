821c876b3c6c8f194991902dffb0a5b220b2088c
bluwolfer777 <leonleonleonr@gmail.com>
2024-03-12 12:58:45 +0100

    created base question class

---
+++ web/QuizMania/question.py
@@ -0,0 +1,7 @@
+
+class Question:
+    def __init__(self, text, answers, rightAnswer, difficulty):
+        self.text = text
+        self.answers = answers
+        self.rightAnswer = rightAnswer
+        self.difficulty = difficulty
