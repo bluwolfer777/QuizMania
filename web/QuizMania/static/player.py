abbe785a94203fe626d2a125aa225cbd85145112
Kirbyy21 <cortesiruben944@gmail.com>
2024-03-12 15:40:30 +0100

    Class Player in progress (not really sure if is going to work)

--- web/QuizMania/static/player.py
+++ web/QuizMania/static/player.py
@@ -1,12 +1,56 @@
 class Player:
-    def __init__(self, name, surname, point):
+
+    def __init__(self, name, surname, addpointer):
         self.name = name
         self.surname = surname
-        self.point = point
+        self.points = addpointer
+        self.addpointer = addpointer
+
+    def add_points(self, points):
+        self.points += points
+        return self.points
 
     def __str__(self):
-        username = self.name + "." + self.surname
-        return f"{username} - {self.point}"
-    
-try1 = Player("Giorgio", "Giovanna", 69)
+        username = f"{self.name}.{self.surname}"
+        if (self.points == 0):
+            return f"{username}"
+        else:    
+            return f"{username} - {self.points}"
+
+try1 = Player("Bobo", "Debobbis", 0)
+try1.add_points(0)
 print(try1)
+
+class Playyer:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, username):
+        #if username not in self.users:
+        self.users[username] = 0
+        print(f"{username} è entrato nella stanza")
+
+    def remove_user(self, username):
+        #if username in self.users:
+        del self.users[username]
+        print(f"{username} ha lasciato la stanza")
+
+    def add_points(self, username, points):
+        #if username in self.users:
+        self.users[username] += points
+
+    def get_user_points(self, username):
+        return self.users.get(username, 0)
+
+    def __str__(self):
+        return f"Utenti: {', '.join(self.users.keys())}"
+
+room = Playyer()
+user1 = "Bobo"
+room.add_user(user1)
+
+print(room)
+
+room.add_points(user1, 10)
+
+print(f"{user1}: {room.get_user_points(user1)}")
