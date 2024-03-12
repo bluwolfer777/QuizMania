a98d1e7d47e383c3c53a757c8b1f6641957b2be2
bluwolfer777 <leonleonleonr@gmail.com>
2024-03-12 13:40:30 +0100

    enabled session id hash

--- web/QuizMania/question.py
+++ web/QuizMania/question.py
@@ -1,7 +1,11 @@
-
+import random
 class Question:
     def __init__(self, text, answers, rightAnswer, difficulty):
         self.text = text
         self.answers = answers
         self.rightAnswer = rightAnswer
         self.difficulty = difficulty
+
+    def getMixedAswers(self):
+        random.shuffle(self.answers)
+        return self.answers
