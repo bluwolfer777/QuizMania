0d4fe0dd21e9dea4af66f486a1116e101d11a3a1
Kirbyy21 <cortesiruben944@gmail.com>
2024-03-13 10:11:19 +0100

    Playyer class complete (i guess)

--- web/QuizMania/static/player.py
+++ web/QuizMania/static/player.py
@@ -1,56 +1,23 @@
-class Player:
-
-    def __init__(self, name, surname, addpointer):
-        self.name = name
-        self.surname = surname
-        self.points = addpointer
-        self.addpointer = addpointer
-
-    def add_points(self, points):
-        self.points += points
-        return self.points
-
-    def __str__(self):
-        username = f"{self.name}.{self.surname}"
-        if (self.points == 0):
-            return f"{username}"
-        else:    
-            return f"{username} - {self.points}"
-
-try1 = Player("Bobo", "Debobbis", 0)
-try1.add_points(0)
-print(try1)
-
 class Playyer:
     def __init__(self):
         self.users = {}
 
-    def add_user(self, username):
-        #if username not in self.users:
+    def add_user(self, name, surname):
+        username = f"{name}.{surname}"
         self.users[username] = 0
         print(f"{username} è entrato nella stanza")
 
     def remove_user(self, username):
-        #if username in self.users:
         del self.users[username]
         print(f"{username} ha lasciato la stanza")
 
     def add_points(self, username, points):
-        #if username in self.users:
         self.users[username] += points
 
     def get_user_points(self, username):
         return self.users.get(username, 0)
 
     def __str__(self):
-        return f"Utenti: {', '.join(self.users.keys())}"
-
-room = Playyer()
-user1 = "Bobo"
-room.add_user(user1)
-
-print(room)
-
-room.add_points(user1, 10)
-
-print(f"{user1}: {room.get_user_points(user1)}")
+        sorted_users = sorted(self.users.items(), key=lambda x: x[1], reverse=True)
+        user_points_str = "\n".join([f"{user}: {points}" for user, points in sorted_users])
+        return f"Classifica:\n{user_points_str}"
